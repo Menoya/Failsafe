8b18c6ad47ba5c8afa0e2a8467ffbcab0c643781
RMCall (Ildar) <mylashev@gmail.com>
2025-04-26 23:51:59 +0300

    рефакторинг реализаций EventBus

--- Assets/Failsafe/Scripts/Character/Components/CharacterEventBus.cs
+++ Assets/Failsafe/Scripts/Character/Components/CharacterEventBus.cs
@@ -1,9 +1,18 @@
-﻿using Failsafe.Scripts.Infrastructure.EventBus;
+﻿using System;
+using Failsafe.Scripts.Infrastructure.EventBus;
+using Failsafe.Scripts.Infrastructure.EventBus.Events;
 
 namespace Failsafe.Scripts.Character.Components
 {
     public class CharacterEventBus : EventBus
     {
+        public new void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : struct, ICharacterEvent => 
+            base.Subscribe(handler);
         
+        public new void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : struct, ICharacterEvent => 
+            base.Unsubscribe(handler);
+        
+        public new void Publish<TEvent>(TEvent evt) where TEvent : struct, ICharacterEvent => 
+            base.Publish(evt);
     }
 }
--- Assets/Failsafe/Scripts/Services/Input/Character/CharacterInputEventBus.cs
+++ Assets/Failsafe/Scripts/Services/Input/Character/CharacterInputEventBus.cs
@@ -1,6 +1,18 @@
-﻿using Failsafe.Scripts.Infrastructure.EventBus;
+﻿using System;
+using Failsafe.Scripts.Infrastructure.EventBus;
+using Failsafe.Scripts.Infrastructure.EventBus.Events;
 
 namespace Failsafe.Scripts.Services.Input.Character
 {
-    public class CharacterInputEventBus : EventBus { }
+    public class CharacterInputEventBus : EventBus
+    {
+        public new void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : struct, IInputEvent => 
+            base.Subscribe(handler);
+        
+        public new void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : struct, IInputEvent => 
+            base.Unsubscribe(handler);
+        
+        public new void Publish<TEvent>(TEvent evt) where TEvent : struct, IInputEvent => 
+            base.Publish(evt);
+    }
 }
