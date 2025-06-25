0e59a55eb62db5eae272d1a1fd273186304b2e70
GeroiGorodskoyZastroiki <denhardx@gmail.com>
2025-06-25 15:47:28 +0300

    player health + injected inspector

--- Assets/Failsafe/Entites/Items/!Components/Scripts/FieldModifiers/Permanent/Healer/Healer.cs
+++ Assets/Failsafe/Entites/Items/!Components/Scripts/FieldModifiers/Permanent/Healer/Healer.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
+using Failsafe.Scripts.Health;
+using VContainer;
 
 public class Healer : MonoBehaviour
 {
     public HealData Data;
+    [Inject] private IHealth _health;
 
-    public void Heal()
-    {
-        //GetComponentInParent<PlayerHealth>().Health += HealData.HealAmount;
-        Debug.Log("Healed");
-    }
+    public void Heal() =>
+        _health.AddHealth(Data.HealAmount);
 }
