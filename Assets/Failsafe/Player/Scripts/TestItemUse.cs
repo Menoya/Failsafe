d1178db44b8f8fd84e0624a142f01ea0df185518
TrofimovEI <trofimoved@mail.ru>
2025-06-30 20:58:53 +0500

    Рефакторинг хилящего предмета
    Отделил логику предмета от визуала.
    
    Добавил скрипт Player/Scripts/TestItemUse для тестирования предметов без спауна и подбора.
    Нужно перетащить его на игрока. В выпадающем списке ItemName выбрать нужный предмет (либо в ItemPrefab перетащить нужный предмет) и нажать E чтобы сработал эффект.

---
+++ Assets/Failsafe/Player/Scripts/TestItemUse.cs
@@ -0,0 +1,70 @@
+using Failsafe.Items;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VContainer;
+
+namespace Failsafe.Player
+{
+    /// <summary>
+    /// Используется для тестирования логики предметов без необходимости их спаунить и подбирать
+    /// </summary>
+    public class TestItemUse : MonoBehaviour
+    {
+        [Inject] private IEnumerable<IUsable> _items;
+        
+        [ValueDropdown("_itemNames")]
+        public string ItemName;
+        public GameObject ItemPrefab;
+        public string UseOnKey = "E";
+
+        private string[] _itemNames;
+        private KeyCode _useKeyCode;
+
+        [ContextMenu(nameof(TestUse))]
+        public void TestUse()
+        {
+            string itemName = string.Empty;
+            if (ItemPrefab)
+            {
+                itemName = ItemPrefab.name;
+            }
+            else
+            {
+                itemName = ItemName;
+            }
+
+            if (string.IsNullOrEmpty(itemName))
+            {
+                Debug.Log($"({nameof(TestItemUse)}) Не задан предмет для теста");
+                return;
+            }
+            var item = _items.FirstOrDefault(x => x.GetType().Name == itemName);
+            if (item == null)
+            {
+                Debug.Log($"({nameof(TestItemUse)}) Не найдена реализация для {itemName}");
+                return;
+            }
+            item.Use();
+        }
+
+        void Start()
+        {
+            _itemNames = _items.Select(x => x.GetType().Name).Concat(new string[1] { "" }).ToArray();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(_useKeyCode))
+            {
+                TestUse();
+            }
+        }
+
+        void OnValidate()
+        {
+            _useKeyCode = System.Enum.Parse<KeyCode>(UseOnKey);
+        }
+    }
+}
