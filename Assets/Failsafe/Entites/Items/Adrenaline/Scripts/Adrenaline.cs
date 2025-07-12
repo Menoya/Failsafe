b77b935d6225a05c78d8d1e3061437a11fe55302
TrofimovEI <trofimoved@mail.ru>
2025-07-12 15:41:13 +0500

    Переделал эффект от Адреналина на IEffectManager
    Переписал эффект от Адреналина чтобы использовалась Система эффектов

--- Assets/Failsafe/Entites/Items/Adrenaline/Scripts/Adrenaline.cs
+++ Assets/Failsafe/Entites/Items/Adrenaline/Scripts/Adrenaline.cs
@@ -1,42 +1,60 @@
-using Failsafe.Player.Model;
-using UnityEngine;
 using Failsafe.Scripts.Modifiebles;
-using System.Collections;
-using System;
+using Failsafe.Scripts.EffectSystem;
+using Failsafe.PlayerMovements;
 
 namespace Failsafe.Items
 {
-    public class Adrenaline : IUsable, ILimitedEffect
+    public class Adrenaline : IUsable
     {
-        AdrenalineData _data;
-        private PlayerMovements.PlayerMovementParameters _playerMovementParameters;
-        private IModificator<float> _speedModificator;
+        private readonly AdrenalineData _data;
+        private readonly IEffectManager _effectManager;
 
-        public Adrenaline(AdrenalineData data, PlayerMovements.PlayerMovementParameters playerMovementParameters)
+        //Т.к. эффект уникальный, можно создать его один раз и не пересоздавать при каждом применении
+        private readonly AdrenalineEffect _effect;
+
+
+        public Adrenaline(AdrenalineData data, PlayerMovementParameters playerMovementParameters, IEffectManager effectManager)
         {
             _data = data;
-            _playerMovementParameters = playerMovementParameters;
-            _speedModificator = new MultiplierFloat(_data.SpeedMultiplier, priority: 100);
+            _effectManager = effectManager;
+            _effect = new AdrenalineEffect(playerMovementParameters, data);
         }
-
-        public IEnumerator EndEffect(Action callback)
-        {
-            yield return new WaitForSeconds(_data.Duration);
-            _playerMovementParameters.WalkSpeed.RemoveModificator(_speedModificator);
-            _playerMovementParameters.RunSpeed.RemoveModificator(_speedModificator);
-            _playerMovementParameters.CrouchSpeed.RemoveModificator(_speedModificator);
-
-            callback.Invoke();
-        }
-
 
         public void Use()
         {
-            //происходит именно УМНОЖЕНИЕ
-            _playerMovementParameters.WalkSpeed.AddModificator(_speedModificator);
-            _playerMovementParameters.RunSpeed.AddModificator(_speedModificator);
-            _playerMovementParameters.CrouchSpeed.AddModificator(_speedModificator);
+            // Если эффекты должны складываться друг с другом, то нужно убрать у эффекта IsUniqueEffect = true;
+            // и создавать новый экземпляр перед каждым применением (в этом случае можно создать Pooling для оптимизации)
+            _effectManager.ApplyEffect(_effect);
+        }
 
+        //TODO: Вынести в отдельный класс если эффект будет переиспользоваться другими предметами
+        public class AdrenalineEffect : Effect
+        {
+            private readonly PlayerMovementParameters _playerMovementParameters;
+            private readonly IModificator<float> _speedModificator;
+
+            public AdrenalineEffect(PlayerMovementParameters playerMovementParameters, AdrenalineData data)
+            {
+                _playerMovementParameters = playerMovementParameters;
+                _speedModificator = new MultiplierFloat(data.SpeedMultiplier, priority: 100);
+                _duration = data.Duration;
+                IsUniqueEffect = true;
+            }
+
+            public override void ApplyEffect()
+            {
+                //происходит именно УМНОЖЕНИЕ
+                _playerMovementParameters.WalkSpeed.AddModificator(_speedModificator);
+                _playerMovementParameters.RunSpeed.AddModificator(_speedModificator);
+                _playerMovementParameters.CrouchSpeed.AddModificator(_speedModificator);
+            }
+
+            public override void ClearEffect()
+            {
+                _playerMovementParameters.WalkSpeed.RemoveModificator(_speedModificator);
+                _playerMovementParameters.RunSpeed.RemoveModificator(_speedModificator);
+                _playerMovementParameters.CrouchSpeed.RemoveModificator(_speedModificator);
+            }
         }
     }
 }
